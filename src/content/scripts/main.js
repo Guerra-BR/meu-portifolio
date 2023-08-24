a989c68401b4f9f7f06da2a1dfa413436b02b650
Guerino Henrique <guerinohenrique@gmail.com>
2023-08-24 19:56:19 -0300

    ajustes, animacao type

--- src/content/scripts/main.js
+++ src/content/scripts/main.js
@@ -1,49 +1,50 @@
 document.addEventListener('DOMContentLoaded', function(){
-    
-    document.addEventListener('mousemove', function(e){
-        const blob = document.getElementById("seguidor")
-        var x = e.clientX
-        var y = e.clientY
-    
-        blob.style.display = 'block'
-        blob.classList.add('show')
+  
+  document.addEventListener('mousemove', function(e){
 
-        blob.animate({
-            left: x + 'px',
-            top: y + 'px'
-        }, {duration: 5000, fill: "forwards"})
+    const blob = document.getElementById("seguidor")
+    blob.classList.add('anim-aparecer-rotate')
+    var x = e.clientX
+    var y = e.clientY
 
+    blob.style.display = 'block'
+    blob.classList.add('show')
+
+    blob.animate({
+        left: x + 'px',
+        top: y + 'px'
+    }, {duration: 5000, fill: "forwards"})
+  })
+
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+  let interval = null;
+  
+  document.querySelector("h1").onmouseover = event => {  
+    let iteration = 0;
+    
+    clearInterval(interval);
+    
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if(index < iteration) {
+            return event.target.dataset.value[index];
+          }
         
-    })
-    
-    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    
-    let interval = null;
-    
-    document.querySelector("h1").onmouseover = event => {  
-      let iteration = 0;
+          return letters[Math.floor(Math.random() * 26)]
+        })
+        .join("");
       
-      clearInterval(interval);
+      if(iteration >= event.target.dataset.value.length){ 
+        clearInterval(interval);
+      }
       
-      interval = setInterval(() => {
-        event.target.innerText = event.target.innerText
-          .split("")
-          .map((letter, index) => {
-            if(index < iteration) {
-              return event.target.dataset.value[index];
-            }
-          
-            return letters[Math.floor(Math.random() * 26)]
-          })
-          .join("");
-        
-        if(iteration >= event.target.dataset.value.length){ 
-          clearInterval(interval);
-        }
-        
-        iteration += 1 / 3;
-      }, 30);
-    }
-    
-    
+      iteration += 1 / 3;
+    }, 30);
+  }
+  
+
+  
 })
